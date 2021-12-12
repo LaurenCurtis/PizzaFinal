ea2902caf94ec927a8d48b871140ef3f8a9418a5
LaurenCurtis <lauren_curtis1@baylor.edu>
2021-12-12 17:55:36 -0600

    Adding the comment form

--- pizzas/views.py
+++ pizzas/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Pizza,Topping
+from .forms import CommentForm
 # Create your views here.
 def index(request):
     return render(request,'pizzas/index.html')
@@ -14,3 +15,19 @@ def pizza(request, pizza_id):
     toppings = pizza.topping_set.order_by('name')
     context = {'pizza':pizza, 'toppings':toppings}
     return render(request,'pizzas/pizza.html',context)
+
+def new_comment(request, pizza_id):
+    pizza = Pizza.objects.get(id = pizza_id)
+    if request.method != 'POST':
+        form = CommentForm()
+    else:
+        form = CommentForm(data=request.POST)
+        
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.pizza = pizza
+            new_entry.save()
+            form.save()
+            return redirect('pizzas:pizza', pizza_id=pizza_id)
+    context = {'form':form, 'pizza':pizza}
+    return render(request, 'pizzas/new_comment.html',context)
