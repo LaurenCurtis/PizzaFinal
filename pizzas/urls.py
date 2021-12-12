db8b0650bc35b5300aeba52252263b345a9f887a
LaurenCurtis <lauren_curtis1@baylor.edu>
2021-12-12 17:16:46 -0600

    Adding the individual pizza page with toppings

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -4,5 +4,6 @@ app_name = 'pizzas'
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('pizzas',views.pizzas,name ='pizzas')
+    path('pizzas',views.pizzas,name ='pizzas'),
+    path('pizzas/<int:pizza_id>/',views.pizza,name = 'pizza')
 ]
