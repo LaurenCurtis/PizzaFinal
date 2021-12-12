4c9c6e01368b64ad05ffd1e52f57a8a9d50a02f6
LaurenCurtis <lauren_curtis1@baylor.edu>
2021-12-12 16:39:59 -0600

    Adding topings/comments and registering them

--- pizzas/models.py
+++ pizzas/models.py
@@ -4,4 +4,24 @@ from django.db import models
 class Pizza(models.Model):
     name = models.CharField(max_length=200)
     def __str__(self):
-        return self.text
+        return self.name
+
+class Topping(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'toppings'
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'comments'
+
+    def __str__(self):
+        return self.name
