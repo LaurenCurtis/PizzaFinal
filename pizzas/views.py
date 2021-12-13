e20a95e255841a7a899d1019e735409ddf943cf3
LaurenCurtis <lauren_curtis1@baylor.edu>
2021-12-12 18:12:52 -0600

    Comments saving to pizza

--- pizzas/views.py
+++ pizzas/views.py
@@ -13,6 +13,7 @@ def pizzas(request):
 def pizza(request, pizza_id):
     pizza = Pizza.objects.get(id=pizza_id)
     toppings = pizza.topping_set.order_by('name')
+    comments = pizza.comment_set.order_by('name')
     context = {'pizza':pizza, 'toppings':toppings}
     return render(request,'pizzas/pizza.html',context)
 
